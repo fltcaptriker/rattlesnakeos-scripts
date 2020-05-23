0a02d57aadeb686c9d4ca82ba6a119e7c69fd8d6
fltcaptriker <fltcaptriker@gmail.com>
2020-05-23 00:55:42 -0400

    Update 00001-opengapps-dependencies.sh

--- 00001-opengapps-dependencies.sh
+++ 00001-opengapps-dependencies.sh
@@ -2,3 +2,5 @@
 
 sudo apt-get -y install lzip
 #repo sync $HOME/rattlesnake-os
+cd $HOME/rattlesnake-os
+retry repo init --manifest-url "$MANIFEST_URL" --manifest-branch "$AOSP_BRANCH" --depth 1 || true
